01ce76a3d80414df4080a83e8256ac3722c21c7e
TerryYan26 <103209100+TerryYan26@users.noreply.github.com>
2022-04-24 13:17:53 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -20,8 +20,14 @@ public class MarkdownParse {
             currentIndex = closeParen + 1;
             if(openBracket == -1|| closeBracket ==-1 || openParen ==-1 || closeParen ==-1){
               break;}
+                 //![image] is the image format
+                 if (openBracket != 0 && markdown.charAt(openBracket - 1) == '!') {
+                  currentIndex = closeParen + 1;
+                continue;
         }
-
+        toReturn.add(markdown.substring(openParen + 1, closeParen));
+        currentIndex = closeParen + 1;
+      }
         return toReturn;
     }
 
