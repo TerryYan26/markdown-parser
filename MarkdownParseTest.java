909e7d6f8ffde100c5764012f1d80f832e115c18
TerryYan26 <103209100+TerryYan26@users.noreply.github.com>
2022-05-22 15:53:18 -0700

    update test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -23,7 +23,17 @@ public class MarkdownParseTest {
     private String content4;
     private ArrayList<String> links4;
 
+    private Path fileName5;
+    private String content5;
+    private ArrayList<String> links5;
 
+    private Path fileName6;
+    private String content6;
+    private ArrayList<String> links6;
+
+    private Path fileName7;
+    private String content7;
+    private ArrayList<String> links7;
     @Before
     public void setUp() throws IOException {
         fileName1 = Path.of("test-file.md");
@@ -41,6 +51,18 @@ public class MarkdownParseTest {
         fileName4 = Path.of("testing3.md");
         content4 = Files.readString(fileName4);
         links4 = new ArrayList<String>();
+
+        fileName5 = Path.of("Snippet1.md");
+        content5 = Files.readString(fileName5);
+        links5 = new ArrayList<String>();
+
+        fileName6 = Path.of("Snippet2.md");
+        content6 = Files.readString(fileName6);
+        links6 = new ArrayList<String>();
+
+        fileName7 = Path.of("Snippet3.md");
+        content7 = Files.readString(fileName7);
+        links7 = new ArrayList<String>();
     }
     @Test
     public void addition() {
@@ -77,6 +99,36 @@ public class MarkdownParseTest {
         assertEquals(links4, MarkdownParse.getLinks(content4));
     
     }
+    @Test
+    public void testTestFile5() {
+        links5.add("`google.com");
+        links5.add("google.com");
+        links5.add("ucsd.edu");
+       
+        assertEquals(links5, MarkdownParse.getLinks(content5));
+    
+    }
+    @Test
+    public void testTestFile6() {
+        links5.add("a.com");
+        links5.add("a.com(())");
+        links5.add("example.com");
+       
+        assertEquals(links6, MarkdownParse.getLinks(content6));
+    
+    }
+    @Test
+    public void testTestFile7() {
+        links7.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
+
+       
+        assertEquals(links7, MarkdownParse.getLinks(content7));
+    
+    }
 }
 // javac -cp ".;lib\junit-4.13.2jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
 // java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
+
+//window
+//javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
+//java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
